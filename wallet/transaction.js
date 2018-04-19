dc60c829dfb6dae6d4f5cbc4e992c6cff1c7e2d3
Chux Rincon <00jjessejj00@gmail.com>
2018-04-19 00:54:00 -0400

    Changed transactions and added updates

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -6,6 +6,24 @@ class Transaction {
     this.input = null;
     this.outputs = [];
   }
+
+  update(senderWallet, recipient, amount) {
+    const senderOutput = this.outputs.find(
+      output => output.address === senderWallet.publicKey
+    );
+
+    if (amount > senderOutput.amount) {
+      console.log(`Amount: ${amount} exceeds balance.`);
+      return;
+    }
+
+    senderOutput.amount = senderOutput.amount - amount;
+    this.outputs.push({ amount, address: recipient });
+    Transaction.signTransaction(this, senderWallet);
+
+    return this;
+  }
+
   static newTransaction(senderWallet, recipient, amount) {
     if (amount > senderWallet.balance) {
       console.log(`Amount: ${amount} exceeds balance.`);
@@ -23,8 +41,25 @@ class Transaction {
         { amount, address: recipient }
       ]
     );
+    Transaction.signTransaction(transaction, senderWallet);
 
     return transaction;
+  }
+  static signTransaction(transaction, senderWallet) {
+    transaction.input = {
+      timestamp: Date.now(),
+      amount: senderWallet.balance,
+      address: senderWallet.publicKey,
+      signature: senderWallet.sign(ChainUtil.hash(transaction.outputs))
+    };
+  }
+
+  static verifyTransaction(transaction) {
+    return ChainUtil.verifySignature(
+      transaction.input.address,
+      transaction.input.signature,
+      ChainUtil.hash(transaction.outputs)
+    );
   }
 }
 
